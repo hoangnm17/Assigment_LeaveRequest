ace4800a07a9c39c5d4c90c25b5679fc0ba9bb6a
hoangnguyen175 <hoangzk255@gmail.com>
2025-10-16 23:28:11 +0700

    create list app controller
    create list app controller

---
+++ Assignment_LeaveRequest/src/java/controller/create_request/ListAppController.java
@@ -0,0 +1,40 @@
+package controller.create_request;
+
+import dal.LeaveRequestDBContext;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import models.LeaveRequest;
+
+/**
+ *
+ * @author 84911
+ */
+@WebServlet(name = "ListAppController", urlPatterns = {"/app/list"})
+public class ListAppController extends HttpServlet {
+
+    protected void procesRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LeaveRequestDBContext list_app_db = new LeaveRequestDBContext();
+        ArrayList<LeaveRequest> list_app = list_app_db.list();
+        request.setAttribute("listApp", list_app);
+        request.getRequestDispatcher("../view/leaveRequest/listRequest.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        procesRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        procesRequest(request, response);
+    }
+
+}
